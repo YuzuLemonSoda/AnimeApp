a7ce98b7762b56925c8b56ad62a33092d0d9f99e
Amado Azua III <amado.azua3@gmail.com>
2022-08-19 16:11:17 -0500

    initial commit

---
+++ src/main/java/com/example/AnimeApp/AnimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AnimeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AnimeApp/AnimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnimeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
