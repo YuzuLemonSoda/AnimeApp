6fb6ef6e425bca29f09d14ff2aead8372db60055
Amado Azua III <amado.azua3@gmail.com>
2022-09-05 19:39:07 -0500

    implement Anime entity

---
+++ src/main/java/com/example/AnimeApp/model/Anime.java
@@ -0,0 +1,34 @@
+package com.example.AnimeApp.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class Anime {
+
+    @Id
+    private Long id;
+
+    private String[] altTitles;
+
+    private String[] genres;
+
+    private String thumbnail;
+
+    private String myAnimeListLink;
+
+    private String title;
+
+    private String episodes;
+
+    private String status;
+
+    private String synopsis;
+
+    private String ranking;
+
+    private String mediaType;
+
+    private Boolean hasRanking;
+
+}
